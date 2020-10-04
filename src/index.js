ebdd0bf115cc3126f3f604e367a6accf71a16176
Darya Tsimashenkava (@besovadevka) <timaph@mail.ru>
2020-10-04 12:46:13 +0300

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let newBr = bracketsConfig.join(" ").replace(/,/g, "").split(" ");
+let count = newBr.length;
+for (let h = 0; h <= count; h++) {
+    newBr.forEach ((item) => {
+        while (str.includes(item)) {
+            for (let i = 0; i < newBr.length; i++) {
+                while (str.includes(newBr[i])) {
+                    str = str.replace(newBr[i], "");
+                    console.log(str);
+                }
+            }
+        }
+    })
+}
+if (str === "") {
+    return true;
+} else {
+    return false;
+}
 }
